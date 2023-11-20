f0a0a47d9eb70622166292ffb789c5f7ed63d82b
Dmytro <moiseenkodmitriy@i.ua>
2023-11-20 02:35:54 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-course',
+  base: '/great-react',
   plugins: [react()],
 });
