bf89f256c6309a69b1f2e93768e06349ec79a610
Dmytro <moiseenkodmitriy@i.ua>
2025-02-05 00:35:50 +0200

    deploy

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,24 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { Avatar } from 'antd';
-<<<<<<< HEAD
-import { Wrap, Buton } from './Usermenu.styled';
-import { logOut } from '../../redux/Authorization/operations';
-import { useDispatch } from 'react-redux';
-import { LogoutOutlined } from '@ant-design/icons';
-
-export const UserMenu = () => {
-  const { email } = useSelector(state => state.auth.user);
-  const dispatch = useDispatch();
-
-  return (
-    <Wrap>
-      <Avatar>{email.slice(0, 1).toUpperCase()}</Avatar>
-      <p>{email}</p>
-      <Buton type="default" onClick={() => dispatch(logOut())}>
-        <LogoutOutlined /> Вийти
-      </Buton>
-    </Wrap>
-=======
 import { LogoutOutlined } from '@ant-design/icons';
 import { logOut } from '../../redux/auth/operationsAuth';
 import { selectUser } from '../../redux/auth/authSelectors';
@@ -36,6 +17,5 @@ export const UserMenu = () => {
         <LogoutOutlined /> Вийти
       </ButtonLogout>
     </WrapperUser>
->>>>>>> 08-phonebook(jwt)
   );
 };
