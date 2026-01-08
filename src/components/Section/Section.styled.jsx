5f2dbc856a836cd89398e895876bde2bee8c60b0
Dmytro <moiseenkodmitriy@i.ua>
2026-01-08 20:13:12 +0200

    deploy

--- src/components/Section/Section.styled.jsx
+++ src/components/Section/Section.styled.jsx
@@ -5,7 +5,7 @@ export const SectionStyled = styled.section`
   padding: 40px;
   box-shadow: 0 10px 25px -5px rgba(0, 0, 0, 0.05), 0 8px 10px -6px rgba(0, 0, 0, 0.05);
   margin: 20px auto;
-  border-radius: 8px; /
+  border-radius: 8px;
   background-color: #ffffff;
   display: flex;
   flex-direction: column;
