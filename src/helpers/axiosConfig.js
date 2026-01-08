d621e05cdf6b76cbd0b0644a26fad32a36eaeeb8
Dmytro <moiseenkodmitriy@i.ua>
2026-01-08 08:38:59 +0200

    deploy

--- src/helpers/axiosConfig.js
+++ src/helpers/axiosConfig.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// axios.defaults.baseURL = 'https://phonebook-api-backend-v92w.onrender.com/api';
+axios.defaults.baseURL = 'https://phonebook-api-backend-v92w.onrender.com/api';
 
-axios.defaults.baseURL = 'http://localhost:8080/api';
+// axios.defaults.baseURL = 'http://localhost:8080/api';
 
 export const setAuthHeader = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
